c74a17445f9631ed7282b93b302b3cb2abf97c4b
Gopesh Sharma <gopesh.sharma@gmail.com>
2024-06-11 06:06:22 -0400

    getting started

---
+++ src/main/java/live/learnjava/trainsenquiry/Jrtp701miniprojectTrainEnquiryApplication.java
@@ -0,0 +1,13 @@
+package live.learnjava.trainsenquiry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jrtp701miniprojectTrainEnquiryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jrtp701miniprojectTrainEnquiryApplication.class, args);
+	}
+
+}
